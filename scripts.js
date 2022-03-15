853db0bbd70bd4fd414dc76eb0fba4c150501bf6
Nicolas Villaverde <villaverdenicolaspablo@gmail.com>
2022-03-14 22:43:24 -0300

    Creacion sitio de ofertas

--- scripts.js
+++ scripts.js
@@ -1,64 +1,181 @@
-console.log(`Primera entrega del Proyecto Final`);
+console.log(`Desafio Tema: To do App`);
 
-const listadoDeOfertas = [];
-let cadenaDeOfertas = [];
-let indice = ``;
+crearOferta();
 
-class oferta {
-    constructor(trabajo, modalidad, pais) {
-        this.trabajo = trabajo;
-        this.modalidad = modalidad;
-        this.pais = pais;
+const botonAgregar = document.getElementById(`btn__agregar`);
+botonAgregar.addEventListener(`click`, obtenerDatos);
+
+const botonReset = document.getElementById(`btn__reset`);
+botonReset.addEventListener(`click`, eliminarTodo);
+
+const botonSearch = document.getElementById(`btn__search`);
+botonSearch.addEventListener(`click`, filtrarDatos);
+
+// FUNCION OBTIENE DATOS DEL FORMULARIO, CREA BASE DE DATOS O ACTUALIZA Y DISPARA FUNCION PARA VISUALIZAR OFERTAS
+function obtenerDatos() {
+    const nombre = document.getElementById(`offerName`).value;
+    const descripcion = document.getElementById(`descriptionText`).value;
+    const jornada = document.getElementById(`inputGroupJornada`).value;
+    const lugar = document.getElementById(`inputGroupLugar`).value;
+    const remote = document.getElementById(`flexSwitchRemote`).checked;
+    const division = document.getElementById(`inputGroupDivision`).value;
+    const seniority = document.getElementById(`inputGroupSeniority`).value;
+
+    class oferta {
+        constructor(nombre, descripcion, jornada, lugar, remote, division, seniority) {
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.jornada = jornada;
+            this.lugar = lugar;
+            this.remote = remote;
+            this.division = division;
+            this.seniority = seniority;
+        }
     }
-}
 
-// Ingreso de ofertas y caracteristicas
+    let nuevaOferta = new oferta(nombre, descripcion, jornada, lugar, remote, division, seniority);
 
-while (true) {
-    let ingresoOferta = confirm(`Desea ingresar una nueva oferta?\n Presione Aceptar para continuar o Cancelar para salir`);
-    if (ingresoOferta == true) {
-        const nuevoTrabajo = prompt(`Nombre de la oferta quieres incluir?`).toLowerCase();
-        const nuevaModalidad = prompt(`Modalidad de la oferta quieres incluir?`).toLowerCase();
-        const nuevoPais = prompt(`Pais de la oferta quieres incluir?`).toLowerCase();
-
-        const nuevaOferta = new oferta(nuevoTrabajo, nuevaModalidad, nuevoPais);
-
-        listadoDeOfertas.push(nuevaOferta);
+    if (localStorage.getItem("oferta") === null) {
+        let arrOfertas = [];
+        arrOfertas.push(nuevaOferta);
+        localStorage.setItem("oferta", JSON.stringify(arrOfertas));
     } else {
-        break;
+        let arrOfertas = JSON.parse(localStorage.getItem("oferta"));
+        arrOfertas.push(nuevaOferta);
+        localStorage.setItem("oferta", JSON.stringify(arrOfertas));
+    }
+    crearOferta();
+}
+
+// CREA EN EL DOM LA VISUALIZACION DE LAS OFERTAS
+function crearOferta() {
+    let database = JSON.parse(localStorage.getItem("oferta"));
+    const tarjetas = document.querySelectorAll(".tarjeta__ofertas");
+    for (let i = 0; i < tarjetas.length; ++i) {
+        tarjetas[i].remove();
+    }
+
+    for (let i = 0; i < database.length; i++) {
+        let titulo = database[i].nombre;
+        let descripcion = database[i].descripcion;
+        let propiedad1 = database[i].jornada;
+        let propiedad2 = database[i].lugar;
+        let propiedad3 = database[i].remote;
+        let propiedad4 = database[i].division;
+        let propiedad5 = database[i].seniority;
+
+        const root = document.getElementById(`root`);
+        const nuevoDivTarjeta = document.createElement(`div`);
+        const nuevoDivTexto = document.createElement(`div`);
+        const nuevoH3Titulo = document.createElement(`h3`);
+        const nuevoSpanDescripcion = document.createElement(`span`);
+        const nuevoDivPropiedades = document.createElement(`div`);
+        const nuevoP1 = document.createElement(`p`);
+        const nuevoP2 = document.createElement(`p`);
+        const nuevoP3 = document.createElement(`span`);
+        const nuevoP4 = document.createElement(`p`);
+        const nuevoP5 = document.createElement(`p`);
+
+        nuevoDivTarjeta.className += "tarjeta__ofertas d-flex justify-content-between align-items-center p-4 m-1 rounded-3";
+        nuevoDivPropiedades.className += "d-flex gap-2 align-items-baseline";
+        nuevoP3.className += "badge bg-warning rounded-pill fs-6";
+
+        nuevoH3Titulo.innerText = titulo;
+        nuevoSpanDescripcion.innerText = descripcion;
+        nuevoP1.innerText = propiedad1;
+        nuevoP2.innerText = propiedad2;
+
+        if (propiedad3 == true) {
+            nuevoP3.innerText = "Remote";
+        }
+
+        nuevoP4.innerText = propiedad4;
+        nuevoP5.innerText = propiedad5;
+
+        root.appendChild(nuevoDivTarjeta);
+        nuevoDivTarjeta.appendChild(nuevoDivTexto);
+        nuevoDivTexto.appendChild(nuevoH3Titulo);
+        nuevoDivTexto.appendChild(nuevoSpanDescripcion);
+        nuevoDivTarjeta.appendChild(nuevoDivPropiedades);
+        nuevoDivPropiedades.appendChild(nuevoP1);
+        nuevoDivPropiedades.appendChild(nuevoP2);
+        nuevoH3Titulo.appendChild(nuevoP3);
+        nuevoDivPropiedades.appendChild(nuevoP4);
+        nuevoDivPropiedades.appendChild(nuevoP5);
     }
 }
 
-// Crear indice de cadena de ofertas
+// function botonEliminar() {
+//     const nuevoBtn = document.createElement(`button`);
+//     nuevoBtn.className += "btn-close";
 
-function indiceDeCadena(arr, fn) {
-    indice = ``;
-    const trabajos = arr.map((el) => el.trabajo);
-    fn(trabajos);
-}
+//     nuevoBtn.addEventListener(`click`, (e) => {
+//         const divAEliminar = e.target.parentElement;
+//         const root = document.getElementById(`root`);
 
-function crearIndice(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        indice += `${i + 1}: ${arr[i]}\n`;
+//         root.removeChild(divAEliminar);
+//     });
+//     return nuevoBtn;
+// }
+
+// OBTIENE DATOS DEL LOCAL STORAGE, ELIMINA TODO Y ACTULIZA EL LOCAL STORAGE, LUEGO ELIMINA TODO ELEMENTO DEL DOM
+function eliminarTodo() {
+    let arrOfertas = JSON.parse(localStorage.getItem("oferta"));
+
+    arrOfertas.splice(0);
+
+    localStorage.setItem("oferta", JSON.stringify(arrOfertas));
+
+    const tarjetas = document.querySelectorAll(".tarjeta__ofertas");
+    for (let i = 0; i < tarjetas.length; ++i) {
+        tarjetas[i].remove();
     }
 }
-indiceDeCadena(listadoDeOfertas, crearIndice);
 
-// Eliminar ofertas y mostrar ofertas disponibles
+// function tacharActividad() {
+//     const tilde = document.createElement(`input`);
+//     tilde.setAttribute("type", "checkbox");
 
-if (listadoDeOfertas.length > 0) {
-    let eliminarActividad = confirm(`Las ofertas de trabajo ingresadas son \n${indice}. Deseas eliminar alguna?\n Ingrese SI o NO`);
+//     tilde.addEventListener(`change`, (e) => {
+//         const spanAModificar = tilde.parentNode.querySelector("span");
+//         if (e.target.checked) {
+//             spanAModificar.className += "text-decoration-line-through fst-italic";
+//             console.log(spanAModificar);
+//         } else {
+//             spanAModificar.className = ``;
+//         }
+//     });
+//     return tilde;
+// }
 
-    if (eliminarActividad == true) {
-        let ofertasAEliminar = parseInt(prompt(`Qué ofertas de trabajo quieres eliminar? ${indice}\n Ingrese el numero de la actividad`));
-        ofertasAEliminar--;
+function filtrarDatos() {
+    let arrOfertas = JSON.parse(localStorage.getItem("oferta"));
+    //console.log(arrOfertas);
+    const busqueda = "Front end";
+    //console.log(busqueda);
+    console.log("apretamo");
+    arrFiltrado = [];
 
-        listadoDeOfertas.splice(ofertasAEliminar, 1);
-        indiceDeCadena(listadoDeOfertas, crearIndice);
-        alert(`Las ofertas disponibles son \n${indice}`);
-    } else {
-        alert(`Las ofertas disponibles son \n${indice}`);
-    }
-} else {
-    alert(`No hay ofertas disponibles`);
+    arrOfertas.forEach((object) => {
+        if (object.nombre === "Front end") {
+            arrFiltrado.push(object);
+        }
+        return arrFiltrado;
+    });
+
+    console.log(arrFiltrado);
+
+    //let arrFiltrado = arrOfertas.filter((e) => e.nombre == busqueda);
+
+    //console.log(arrFiltrado);
+
+    // for (let i = 0; i < arrOfertas.length; ++i) {
+    //     let objeto = arrOfertas[i];
+
+    //     let results = objeto.filter(function (e) {
+    //         return e.nombre == busqueda;
+    //     });
+
+    //     console.log(results);
+    // }
 }
