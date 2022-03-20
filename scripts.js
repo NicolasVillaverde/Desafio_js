038576019313cbbfc26064de5bd392bdb029fb9c
Nicolas Villaverde <villaverdenicolaspablo@gmail.com>
2022-03-19 22:05:14 -0300

    Se corrige bug de inicio

--- scripts.js
+++ scripts.js
@@ -57,54 +57,58 @@ function crearOferta() {
     for (let i = 0; i < tarjetas.length; ++i) {
         tarjetas[i].remove();
     }
+    if (database.length === null){
+        break
+    }else{
 
-    for (let i = 0; i < database.length; i++) {
-        let titulo = database[i].nombre;
-        let descripcion = database[i].descripcion;
-        let propiedad1 = database[i].jornada;
-        let propiedad2 = database[i].lugar;
-        let propiedad3 = database[i].remote;
-        let propiedad4 = database[i].division;
-        let propiedad5 = database[i].seniority;
-
-        const root = document.getElementById(`root`);
-        const nuevoDivTarjeta = document.createElement(`div`);
-        const nuevoDivTexto = document.createElement(`div`);
-        const nuevoH3Titulo = document.createElement(`h3`);
-        const nuevoSpanDescripcion = document.createElement(`span`);
-        const nuevoDivPropiedades = document.createElement(`div`);
-        const nuevoP1 = document.createElement(`p`);
-        const nuevoP2 = document.createElement(`p`);
-        const nuevoP3 = document.createElement(`span`);
-        const nuevoP4 = document.createElement(`p`);
-        const nuevoP5 = document.createElement(`p`);
-
-        nuevoDivTarjeta.className += "tarjeta__ofertas d-flex justify-content-between align-items-center p-4 m-1 rounded-3";
-        nuevoDivPropiedades.className += "d-flex gap-2 align-items-baseline";
-        nuevoP3.className += "badge bg-warning rounded-pill fs-6";
-
-        nuevoH3Titulo.innerText = titulo;
-        nuevoSpanDescripcion.innerText = descripcion;
-        nuevoP1.innerText = propiedad1;
-        nuevoP2.innerText = propiedad2;
-
-        if (propiedad3 == true) {
-            nuevoP3.innerText = "Remote";
+        for (let i = 0; i < database.length; i++) {
+            let titulo = database[i].nombre;
+            let descripcion = database[i].descripcion;
+            let propiedad1 = database[i].jornada;
+            let propiedad2 = database[i].lugar;
+            let propiedad3 = database[i].remote;
+            let propiedad4 = database[i].division;
+            let propiedad5 = database[i].seniority;
+    
+            const root = document.getElementById(`root`);
+            const nuevoDivTarjeta = document.createElement(`div`);
+            const nuevoDivTexto = document.createElement(`div`);
+            const nuevoH3Titulo = document.createElement(`h3`);
+            const nuevoSpanDescripcion = document.createElement(`span`);
+            const nuevoDivPropiedades = document.createElement(`div`);
+            const nuevoP1 = document.createElement(`p`);
+            const nuevoP2 = document.createElement(`p`);
+            const nuevoP3 = document.createElement(`span`);
+            const nuevoP4 = document.createElement(`p`);
+            const nuevoP5 = document.createElement(`p`);
+    
+            nuevoDivTarjeta.className += "tarjeta__ofertas d-flex justify-content-between align-items-center p-4 m-1 rounded-3";
+            nuevoDivPropiedades.className += "d-flex gap-2 align-items-baseline";
+            nuevoP3.className += "badge bg-warning rounded-pill fs-6";
+    
+            nuevoH3Titulo.innerText = titulo;
+            nuevoSpanDescripcion.innerText = descripcion;
+            nuevoP1.innerText = propiedad1;
+            nuevoP2.innerText = propiedad2;
+    
+            if (propiedad3 == true) {
+                nuevoP3.innerText = "Remote";
+            }
+    
+            nuevoP4.innerText = propiedad4;
+            nuevoP5.innerText = propiedad5;
+    
+            root.appendChild(nuevoDivTarjeta);
+            nuevoDivTarjeta.appendChild(nuevoDivTexto);
+            nuevoDivTexto.appendChild(nuevoH3Titulo);
+            nuevoDivTexto.appendChild(nuevoSpanDescripcion);
+            nuevoDivTarjeta.appendChild(nuevoDivPropiedades);
+            nuevoDivPropiedades.appendChild(nuevoP1);
+            nuevoDivPropiedades.appendChild(nuevoP2);
+            nuevoH3Titulo.appendChild(nuevoP3);
+            nuevoDivPropiedades.appendChild(nuevoP4);
+            nuevoDivPropiedades.appendChild(nuevoP5);
         }
-
-        nuevoP4.innerText = propiedad4;
-        nuevoP5.innerText = propiedad5;
-
-        root.appendChild(nuevoDivTarjeta);
-        nuevoDivTarjeta.appendChild(nuevoDivTexto);
-        nuevoDivTexto.appendChild(nuevoH3Titulo);
-        nuevoDivTexto.appendChild(nuevoSpanDescripcion);
-        nuevoDivTarjeta.appendChild(nuevoDivPropiedades);
-        nuevoDivPropiedades.appendChild(nuevoP1);
-        nuevoDivPropiedades.appendChild(nuevoP2);
-        nuevoH3Titulo.appendChild(nuevoP3);
-        nuevoDivPropiedades.appendChild(nuevoP4);
-        nuevoDivPropiedades.appendChild(nuevoP5);
     }
 }
 
